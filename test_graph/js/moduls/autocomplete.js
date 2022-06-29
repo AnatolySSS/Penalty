385bf4d42f7c3a0ae4cfe72772ea30868aa95c1d
Anatoly <Anatoly.Shilyaev@gmail.com>
2022-06-29 12:25:54 +0300

    [all decision]
    * add preambula
    * add main claims
    * add fo automating list
    * add validation (preambula & main claims)

--- test_graph/js/moduls/autocomplete.js
+++ test_graph/js/moduls/autocomplete.js
@@ -1,4 +1,6 @@
-function autocomplete(inp, arr) {
+import { fo_data } from './objects/foData';
+
+export function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
@@ -63,6 +65,17 @@ function autocomplete(inp, arr) {
           /*and simulate a click on the "active" item:*/
           if (x) x[currentFocus].click();
         }
+        var fo_name = document.querySelector("#fo_name").value
+        fo_data.fo_data.forEach(element => {
+          if (fo_name == element.fo_name) {
+            document.querySelector("#fo_inn").value = element.fo_inn
+            document.querySelector("#fo_post_address").value = element.fo_post_address
+            document.querySelector("#fo_registration_address").value = element.fo_registration_address
+            document.querySelector("#fo_registration_date").value = element.fo_registration_date
+            document.querySelector("#fo_site_check").innerHTML = "Ссылка на сайт ЦБ РФ для проверки ФО"
+            document.querySelector("#fo_site_check").href = "https://cbr.ru/finorg/foinfo/?ogrn=" + element.fo_ogrn
+          }
+        });
       }
   });
   function addActive(x) {
