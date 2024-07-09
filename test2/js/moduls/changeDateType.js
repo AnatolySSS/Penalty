8911e0f98f892a3cf8fca2788adccd0aac9c0d87
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-07-09 19:35:05 +0300

    [base] delete old versions

--- test2/js/moduls/changeDateType.js
+++
@@ -1,5 +0,0 @@
-//Change date type from 01.02.2019 to 2019-02-01
-export function changeDateType(date){
-  date = date.slice(6, 10) + "-" + date.slice(3, 5) + "-" + date.slice(0, 2);
-  return date;
-}
